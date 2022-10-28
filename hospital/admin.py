1cce4c58f73dd8e1676b0f4855f5739276224db7
abhinav <pampatiabhinav2312@gmail.com>
2022-10-29 00:50:31 +0530

    first

---
+++ hospital/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Doctor,Patient,Appointment,PatientDischargeDetails,Report
+# Register your models here.
+class DoctorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Doctor, DoctorAdmin)
+
+class PatientAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Patient, PatientAdmin)
+
+class ReportAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Report, ReportAdmin)
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Appointment, AppointmentAdmin)
+
+class PatientDischargeDetailsAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(PatientDischargeDetails, PatientDischargeDetailsAdmin)
